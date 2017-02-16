6908571c2241b2f41a4b8f009391eae6660d6f6b
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-02-16 09:56:59 +0100

    Service création conversation

--- app/services/CreatConversationServices.js
+++ app/services/CreatConversationServices.js
@@ -1,10 +1,30 @@
 app.factory('CreatConversationService', function($http, $q){
   var API : 'http://preprod.prokonect.fr/';
-  var service = {
+
+  var conversation = {
     response : false,
     GetAll: function (limit) {
-      var defered = $q.defer();
+      var deferred = $q.defer();
+      var url : API + '/Conversation/';
+
+      $http.get(url).then(function(data, status){
+        conversation.response = data;
+        deferred.resolve(service.response);
+      });
+
+      return defered.promise;
+    },
+    GetById: function(id) {
+      var deferred = $q.defer();
+      var url : API + '/Conversation/view' + id;
+
+      $http.get(url)
+      .then(function(data, status) {
+        service.response = data;
+        deferred.resolve(service.response);
+      });
+      return deferred.promise;
     }
   }
-
+  return conversation;
 });
