bfdeccea06c9a0f55cbfc355c4c7a4cda4da5a9d
davinaz <droulier@gmail.com>
2017-02-16 15:08:49 +0100

    Inscription et logins fonctionnels

--- app/app.routes.js
+++ app/app.routes.js
@@ -8,11 +8,10 @@ appchat.config(function($routeProvider, $locationProvider) {
             templateUrl: 'app/views/signup.html',
             controller: 'UserController'
         })
-        /*.when('/profiles', {
-            templateUrl: 'app/views/profil.html',
-            controller: 'SignupController'
-        })*/
-
+        .when('/login', {
+            templateUrl: 'app/views/login.html',
+            controller: 'UserController'
+        })
         .when('/CreatConversationView', {
           templateUrl: 'app/views/CreatConversation/CreatConversationView.html',
           controller: 'CreatConversationController'
