59cc1d0cf79c61c6788e886843b74ad2d3154ddc
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-02-16 11:08:33 +0100

    Modif API, commentaires supprimer.

--- app/controllers/CreatConversationController/CreatConversationController.js
+++ app/controllers/CreatConversationController/CreatConversationController.js
@@ -1,6 +1,5 @@
 appchat.controller('CreatConversationController', function($scope, CreatConversationService) {
 	CreatConversationService.GetAll(1).then(function(response) {
 		$scope.CreatConversation = response.data;
-		// console.log(response);
 	});
 });
