607adf164b24033c695ce59599cc7f696718ceb2
Death <laurent.chauveau@yahoo.fr>
2017-02-16 13:44:53 +0100

    Correction terme

--- app/controllers/CreatConversationController.js
+++
@@ -1,8 +0,0 @@
-appchat.controller('CreatConversationController', function($scope,$routeParams, CreatConversationService) {
-
-	CreatConversationService.creatConversation = then(function(response) {
-		var creatConversation = {
-				user_id_sender: $scope.user_id_sender,
-				$user_id_recipient: $scope.$user_id_recipient
-	});
-});
--- app/services/CreateConversationServices.js
+++ app/services/CreateConversationServices.js
@@ -1,9 +1,9 @@
-app.factory('CreatConversationService', function($http){
-  var API : 'http://preprod.prokonect.fr/api';
+appchat.factory('CreateConversationService', function($http) {
+  var API = 'http://preprod.prokonect.fr/api';
 
     var service = {
 
-        createConversation: function(CreatConversation) {
+        createConversation: function(user) {
             var url = API + "/conversations/create/" +user.id.recipient+ "/" +user.id.sender;
 
             $http.get(url)
