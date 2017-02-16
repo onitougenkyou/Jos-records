ec204dd8d6f865f928b48d461cdef5471da6b5d0
LAPTOP-NIJDM9S8\death <laurent.chauveau@yahoo.fr>
2017-02-16 09:40:43 +0100

    Début création conversation

---
+++ app/services/CreatConversationServices.js
@@ -0,0 +1,10 @@
+app.factory('CreatConversationService', function($http, $q){
+  var API : 'http://preprod.prokonect.fr/';
+  var service = {
+    response : false,
+    GetAll: function (limit) {
+      var defered = $q.defer();
+    }
+  }
+
+});
