b5d87ff38df460535c168ddab023defe91d215fb
argetkill <argetkill>
2017-02-17 11:59:41 +0100

    vos

--- app/controllers/chat/conversationController.js
+++ app/controllers/chat/conversationController.js
@@ -1,9 +1,33 @@
-appchat.controller('conversationController', function($scope, conversationService,$routeParams) {
+appchat.controller('conversationController', function($scope, conversationService,$routeParams)
+{
   var id = $routeParams.id;
+  var lastID = null;
+  var done = false;
   console.log(id);
 
-    conversationService.getconversation(id).then(function(response) {
-      $scope.messages = response.data.messages;
-      console.log(response.data.messages);
+  conversationService.getconversation(id).then(function(response)
+  {
+    $scope.messages= messages = response.data.messages;
+    for (var i = 0; i < messages.length; i++)
+    {
+      lastID = messages[i].id;
+    }
+    console.log(response.data.messages);
+    console.log("id "+lastID);
+    done = true;
+
+
+  });
+  while(done == true)
+  {
+
+    console.log("last "+lastID);
+    setInterval(function(){
+    conversationService.RefreshById(lastID).then(function(response)
+    {
+      $scope.messages= response.data.messages;
     });
+  }, 10000);
+
+  }
 });
